8d03d532abba30b88a42804c3d2a0cc6346b131c
Allan <ssebattaallan9@gmail.com>
2026-04-10 12:13:29 +0300

    Add movie recommendations, similar movies, and Wikipedia summary endpoints to MovieViewSet

--- backend/movies/views.py
+++ backend/movies/views.py
@@ -16,3 +16,44 @@ from .services.tmdb_service import TMDBService, MovieSyncService, WikipediaServi
 logger = logging.getLogger(__name__)
 tmdb = TMDBService()
 sync_service = MovieSyncService()
+
+## Movie ViewSet
+class MovieViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Movie.objects.prefetch_related("genres", "directors").all()
+    permission_classes = [AllowAny]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["genres__slug"]
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return MovieDetailSerializer
+        return MovieCompactSerializer
+
+    @action(detail=True, methods=["get"])
+    def recommendations(self, request, pk=None):
+        movie = self.get_object()
+        data = tmdb.get_movie_recommendations(movie.tmdb_id)
+        results = data.get("results", [])
+        serializer = TMDBMovieSerializer(results, many=True)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=["get"])
+    def similar(self, request, pk=None):
+        movie = self.get_object()
+        data = tmdb.get_similar_movies(movie.tmdb_id)
+        results = data.get("results", [])
+        serializer = TMDBMovieSerializer(results, many=True)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=["get"])
+    def wikipedia(self, request, pk=None):
+        movie = self.get_object()
+        year = movie.release_date.year if movie.release_date else None
+        wiki_data = WikipediaService.get_movie_summary(movie.title, year)
+
+        if wiki_data.get("summary"):
+            movie.wikipedia_summary = wiki_data["summary"]
+            movie.wikipedia_url = wiki_data["url"]
+            movie.save(update_fields=["wikipedia_summary", "wikipedia_url"])
+
+        return Response(wiki_data)
