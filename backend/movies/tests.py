ac67070500a68b030b6d65a562677c335dbf5b84
Aloysious <aloysiouskyle@gmail.com>
2026-04-11 17:58:33 +0300

    Commit 2: Add Genre model string representation test

--- backend/movies/tests.py
+++ backend/movies/tests.py
@@ -3,4 +3,8 @@ from movies.models import Genre, Person, Movie, WatchProvider
 
 
 class MoviesModelsTests(TestCase):
-    pass
+
+    def test_genre_str(self):
+        """Test the string representation of a Genre."""
+        genre = Genre(tmdb_id=28, name="Action", slug="action")
+        self.assertEqual(str(genre), "Action")
