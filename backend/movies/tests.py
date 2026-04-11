327946652d64bc1c6d5e383a69bdb29933b5bc3d
Aloysious <aloysiouskyle@gmail.com>
2026-04-11 17:58:48 +0300

    Commit 3: Add Person model string representation test

--- backend/movies/tests.py
+++ backend/movies/tests.py
@@ -8,3 +8,8 @@ class MoviesModelsTests(TestCase):
         """Test the string representation of a Genre."""
         genre = Genre(tmdb_id=28, name="Action", slug="action")
         self.assertEqual(str(genre), "Action")
+
+    def test_person_str(self):
+        """Test the string representation of a Person."""
+        person = Person(tmdb_id=1, name="Christopher Nolan")
+        self.assertEqual(str(person), "Christopher Nolan")
