77b6cee15296342a7fbdef93c53b93234c806457
Musoke Nestroy <franciamusoke@gmail.com>
2026-04-12 19:40:42 +0300

    URL py changes

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,6 +1,10 @@
+from django.urls import path
+from . import views
+
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
+
 router = DefaultRouter()
 router.register(r"watchlist", views.WatchlistViewSet, basename="watchlist")
 
