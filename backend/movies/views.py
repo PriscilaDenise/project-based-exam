a8f5900444812b217ca6a81ab9c9ba5ec68c37f2
Allan <ssebattaallan9@gmail.com>
2026-04-13 12:14:28 +0300

    Add logic to parse movie IDs in compare_movies endpoint

--- backend/movies/views.py
+++ backend/movies/views.py
@@ -392,4 +392,6 @@ def discover_filtered(request):
 @api_view(["GET"])
 @permission_classes([AllowAny])
 def compare_movies(request):
+    ids_str = request.query_params.get("ids", "")
+    ids = [int(i.strip()) for i in ids_str.split(",") if i.strip().isdigit()]
 
